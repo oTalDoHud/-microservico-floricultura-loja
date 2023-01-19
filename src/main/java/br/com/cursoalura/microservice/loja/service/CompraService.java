4f47ed014a913deaa869c3b08b871400d87d9ef0
oTalDoHud <hudson.lucas.vieira@gmail.com>
2023-01-19 15:17:15 -0300

    integrando com papertrail

--- src/main/java/br/com/cursoalura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/cursoalura/microservice/loja/service/CompraService.java
@@ -5,26 +5,33 @@ import br.com.cursoalura.microservice.loja.model.Compra;
 import br.com.cursoalura.microservice.loja.model.dto.CompraDTO;
 import br.com.cursoalura.microservice.loja.model.dto.InfoFornecedorDTO;
 import br.com.cursoalura.microservice.loja.model.dto.InfoPedidoDTO;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CompraService {
 
-
+    private static final Logger log = LoggerFactory.getLogger(CompraService.class);
     @Autowired
     private FornecedorClient fornecedorClient;
 
     public Compra realizarCompra(CompraDTO compraDTO) {
-        InfoFornecedorDTO infoFornecedorDTO = fornecedorClient.getInforPorEstado(compraDTO.getEndereco().getEstado());
-        InfoPedidoDTO pedidoDTO = fornecedorClient.realizaPedido(compraDTO.getItens());
 
-        System.out.println(infoFornecedorDTO.getEndereco());
+        final String estado = compraDTO.getEndereco().getEstado();
+        log.info("Buscando informações do fornecedor de {}", estado);
+
+        InfoFornecedorDTO infoFornecedorDTO = fornecedorClient.getInforPorEstado(compraDTO.getEndereco().getEstado());
+
+        log.info("Realizando pedido");
+        InfoPedidoDTO pedidoDTO = fornecedorClient.realizaPedido(compraDTO.getItens());
 
         Compra compraSalva = new Compra();
         compraSalva.setPedidoId(pedidoDTO.getId());
         compraSalva.setTempoDePreparo(pedidoDTO.getTempoDePreparo());
-        compraSalva.setEnderecoDestino(compraDTO.getEndereco().toString());
+        compraSalva.setEnderecoDestino(infoFornecedorDTO.getEndereco().toString());
 
         return compraSalva;
     }
